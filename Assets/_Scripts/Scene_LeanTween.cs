0e7ad92cbb58acf7b3e479b10c90602f8de98156
arya201290 <arya201290@gmail.com>
2015-10-08 17:31:33 -0700

    Summary 1
    Description 1

---
+++ Assets/_Scripts/Scene_LeanTween.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scene_LeanTween : MonoBehaviour {
+
+    public GameObject obj;
+
+	// Use this for initialization
+	void Start ()
+    {
+        print("Start!");
+        if(obj != null)
+        { 
+            LeanTween.move(obj, new Vector3[] { new Vector3(0f, 0f, 0f),
+                                                new Vector3(3.3f, 0f, 3.3f),
+                                                new Vector3(6.6f, 0f, 6.6f),
+                                                new Vector3(10f, 0f, 10f) }, 1.5f)
+                //.setEase(LeanTweenType.easeOutQuad)
+                .setOrientToPath(true);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
---
+++ Assets/_Scripts/Scene_Spline.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scene_Spline : MonoBehaviour {
+
+    public BezierCurve bezierCurve;
+    public BezierPoint bezierPoint;
+
+	// Use this for initialization
+	void Start () {
+        print("Start!");
+
+        print(bezierCurve.pointCount);
+        print(bezierCurve.GetAnchorPoints());
+        print(bezierPoint.position);
+        print(bezierPoint.localPosition);
+
+        print((float)bezierPoint.globalHandle1.x);
+        print((float)bezierPoint.handle1.x);
+        print((float)bezierPoint.globalHandle2.x);
+        print((float)bezierPoint.handle2.x);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
